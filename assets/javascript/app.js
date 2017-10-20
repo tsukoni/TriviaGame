925467c0f36f4e0d9e0d18240c1bb4d6c618e40d
unknown <vuvuong8@aol.com>
2017-10-19 20:46:17 -0700

    first push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,8 @@ var number = 30;
 var intervalId;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var trivia = {
+var currentAnswer;
+/*var trivia = {
 	question: "What fruit is tangy?",
 	choices: [{
 		answer: "Banana",
@@ -17,63 +18,81 @@ var trivia = {
 		answer: "Orange",
 		correct: true
 	}]
-}
-var questionSet = {
-	
-}
+}*/
 $("#time-remaining").html("30 seconds left");
+//default text
+var questionSet = [
+	{question: "Which fruit is tangy?", 
+	options:["Banana","Apple", "Tomato", "Orange"], 
+	answer:"Orange"},
+
+	{question: "What god is not known for thunder?", 
+	options:["Zeus","Thor", "Susanoo", "Shiva"], 
+	answer:"Shiva"},
+
+	{question: "Which rock is the hardest?", 
+	options:["Obsidian","Diamond", "Quartz", "Topaz"], 
+	answer:"Diamond"},
+
+	{question: "What are the 3 wise monkeys not known for?", 
+	options:["See no evil","Hear no evil", "Feel no evil", "Speak no evil"], 
+	answer:"Feel no evil"},
+
+	{question: "What hero has no super powers?", 
+	options:["Iron Man","Green Lantern", "Spider-Man", "Flash"], 
+	answer:"Iron Man"}
+]
+
 function decrement() {
 	number--;
 	$("#time-remaining").html(number + " seconds left");
-	if(number == 0) {
-		stop();
-	}
+	
 }
 function stop() {
       clearInterval(intervalId);
     }
-function loadQuestion(quad) {
-	$("#question").html(quad.question);
-	$("#c-1").html(quad.choices[0].answer);
-	$("#c-1").attr("value", quad.choices[0].correct);
-	$("#c-2").html(quad.choices[1].answer);
-	$("#c-2").attr("value", quad.choices[1].correct);
-	$("#c-3").html(quad.choices[2].answer);
-	$("#c-3").attr("value", quad.choices[2].correct);
-	$("#c-4").html(quad.choices[3].answer);
-	$("#c-4").attr("value", quad.choices[3].correct);
-	$("#score").html("Correct answers: " + correctAnswers + " Incorrect answers " + incorrectAnswers);
+function loadQuestion(x) {
+	number = 30;
+	$("#question").html(questionSet[x].question);
+	currentAnswer = questionSet[x].answer;
+	$("#c-1").html(questionSet[x].options[0]);
+	$("#c-1").attr("value", questionSet[x].options[0]);
+	$("#c-2").html(questionSet[x].options[1]);
+	$("#c-2").attr("value", questionSet[x].options[1]);
+	$("#c-3").html(questionSet[x].options[2]);
+	$("#c-3").attr("value", questionSet[x].options[2]);
+	$("#c-4").html(questionSet[x].options[3]);
+	$("#c-4").attr("value", questionSet[x].options[3]);
+	$("#score").html("Correct answers: " + correctAnswers + " Incorrect answers: " + incorrectAnswers);
+
 }
 function logAnswer() {
 	var namek = $(this).attr("value");
 	console.log(namek);
-	if(namek == false) {
+	if(namek != currentAnswer) {
 		incorrectAnswers++;
 		alert("Incorrect");
-		newQuestion();
-		loadQuestion(trivia);
+		
 	}
 	else {
 		correctAnswers++;
 		alert("Correct");
-		newQuestion();
-		loadQuestion(trivia);
+	
 	}
 }
-function newQuestion(a, b1, b2, c1, c2, d1, d2, e1, e2) {
-	trivia[0] = a;
-	trivia.choices[0].answer = b1;
-	trivia.choices[0].correct = b2;
-	trivia.choices[1].answer = c1;
-	trivia.choices[1].correct = c2;
-	trivia.choices[2].answer = d1;
-	trivia.choices[2].correct = d2;
-	trivia.choices[3].answer = e1;
-	trivia.choices[3].correct = e2;
-}
-intervalId = setInterval(decrement, 1000);
+
+
 $(document).on("click", ".choice", logAnswer);
-loadQuestion(trivia);
+
+for (var i = 0; i < questionSet.length; i++) {
+
+	intervalId = setInterval(decrement, 1000);
+	if(number == 0) {
+		incorrectAnswers++;
+		alert("No answer");
+		loadQuestion(i);
+	}
+}
 
 
 
